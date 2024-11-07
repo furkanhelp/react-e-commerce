cf4ff8f002f133469b6198fc798431b41923a874
Furkan <frknyrdm0@gmail.com>
2024-11-07 21:40:01 +0300

    v0.6

---
+++ src/components/ProductsGrid.jsx
@@ -0,0 +1,30 @@
+import { Link, useLoaderData } from "react-router-dom"
+
+const ProductsGrid = () => {
+  const { products } = useLoaderData();
+
+    return ( 
+    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+    {products.map((products)=> {
+        const {title,price,image} = products.attributes
+        return ( 
+        <Link key={products.id} to={`/products/${products.id}`} className="car
+        w-full shadow-xl hover:shadow-2xl transition duration-300">
+
+        <figure className="px-4 pt-4">
+<img src={image} alt={title} className="rounded-xl h-64 md:h-48 w-full
+    object-cover" />
+        </figure>
+        <div className="car-body items-center text-center">
+            <h2 className="card-title capitalize tracking-wider">
+            {title}
+            </h2>
+            <span className="text-secondary">{price}</span>
+        </div>
+        </Link>
+    );
+    })}
+    </div>
+    );
+}
+export default ProductsGrid
