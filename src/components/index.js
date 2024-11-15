0dc79c5450652ff7f0697e9a29b45f82afc63f26
Furkan <frknyrdm0@gmail.com>
2024-11-15 14:58:14 +0300

    v1.2

--- src/components/index.js
+++ src/components/index.js
@@ -12,5 +12,5 @@ export { default as PaginationContainer } from "./PaginationContainer";
 export { default as Loading } from "./Loading";
 export { default as CartTotals } from "./CartTotals";
 export { default as CartItemsList } from "./CartItemsList";
-
+export { default as CheckoutForm } from "./CheckoutForm";
 
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -5,28 +5,18 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addItem } from "../features/cart/cartSlice";
 
-const singleProductQuery = (id) => {
-  return {
-    queryKey: ["singleProduct", id],
-    queryFn: () => customFetch(`/products/${id}`),
-  };
+export const loader = async ({ params }) => {
+  const response = await customFetch(`/products/${params.id}`);
+
+  return { product: response.data.data };
 };
-
-export const loader =
-  (queryClient) =>
-  async ({ params }) => {
-    const response = await queryClient.ensureQueryData(
-      singleProductQuery(params.id)
-    );
-
-    return { product: response.data.data };
-  };
 
 const SingleProduct = () => {
   const { product } = useLoaderData();
-  const { image, title, price, description, colors, company } =
+  const { image, title, description, price, colors, company } =
     product.attributes;
-  const dollarsAmount = formatPrice(price);
+  const dollarAmount = formatPrice(price);
+
   const [productColor, setProductColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
 
@@ -35,13 +25,14 @@ const SingleProduct = () => {
   };
 
   const cartProduct = {
-    cartID: product.id + productColor,
+    carID: product.id + productColor,
     productID: product.id,
     image,
     title,
     price,
     company,
     productColor,
+    company,
     amount,
   };
 
@@ -71,57 +62,57 @@ const SingleProduct = () => {
           alt={title}
           className="w-96 h-96 object-cover rounded-lg lg:w-full"
         />
-        {/* PRODUCT */}
-        <div>
-          <h1 className="capitalize text-3xl font-bold">{title}</h1>
-          <h4 className="text-xl text-neutral-content font-bold mt-2">
-            {company}
+      </div>
+      {/* PRODUCT */}
+      <div>
+        <h1 className="capitalize text-3xl font-bold">{title}</h1>
+        <h4 className="text-xl text-neutral-content font-bold mt-2">
+          {company}
+        </h4>
+        <p className="mt-3 text-xl">{dollarAmount}</p>
+        <p className="mt-6 leading-8">{description}</p>
+        {/* COLORS */}
+        <div className="mt-6">
+          <h4 className="text-md font-medium tracking-wider capitalize">
+            colors
           </h4>
-          <p className="mt-3 text-xl">{dollarsAmount}</p>
-          <p className="mt-6 leading-8">{description}</p>
-          {/* COLORS */}
-          <div className="mt-6">
-            <h4 className="text-md font-medium tracking-wider capitalize">
-              colors
+          <div className="mt-2">
+            {colors.map((color) => {
+              return (
+                <button
+                  key={color}
+                  type="button"
+                  className={`badge w-6 h-6 mr-2 ${
+                    color === productColor && "border-2 border-secondary"
+                  }`}
+                  style={{ backgroundColor: color }}
+                  onClick={() => setProductColor(color)}
+                ></button>
+              );
+            })}
+          </div>
+        </div>
+        {/* AMOUNT */}
+        <div className="form-control w-full max-w-xs">
+          <label className="label" htmlFor="amount">
+            <h4 className="text-md font-medium -tracking-wider capitalize">
+              amount
             </h4>
-            <div className="mt-2">
-              {colors.map((color) => {
-                return (
-                  <button
-                    key={color}
-                    type="button"
-                    className={`badge w-6 h-6 mr-2 ${
-                      color === productColor && "border-2 border-secondary"
-                    }`}
-                    style={{ backgroundColor: color }}
-                    onClick={() => setProductColor(color)}
-                  ></button>
-                );
-              })}
-            </div>
-          </div>
-          {/* AMOUNT */}
-          <div className="form-control w-full max-w-xs">
-            <label className="label" htmlFor="amount">
-              <h4 className="text-md font-medium -tracking-wider capitalize">
-                amount
-              </h4>
-            </label>
-            <select
-              className="select select-secondary select-bordered select-md"
-              id="amount"
-              value={amount}
-              onChange={handleAmount}
-            >
-              {generateAmountOptions(20)}
-            </select>
-          </div>
-          {/* CART BTN */}
-          <div className="mt-10">
-            <button className="btn btn-secondary btn-md" onClick={addToCart}>
-              Add to bag
-            </button>
-          </div>
+          </label>
+          <select
+            className="select select-secondary select-bordered select-md"
+            id="amount"
+            value={amount}
+            onChange={handleAmount}
+          >
+            {generateAmountOptions(20)}
+          </select>
+        </div>
+        {/* CART BTN */}
+        <div className="mt-10">
+          <button className="btn btn-secondary btn-md" onClick={addToCart}>
+            Add to bag
+          </button>
         </div>
       </div>
     </section>
